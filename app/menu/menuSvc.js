958d5a514b2e24ca91e3df2c6c6e2c236df3cf23
Veeru <veeru.kavula@gmail.com>
2017-10-04 10:10:46 +0530

    modified menuSvc

--- app/menu/menuSvc.js
+++ app/menu/menuSvc.js
@@ -1,58 +1,61 @@
-( function(){
-    
-    function loginSvcFn($http,$q){
-        
-        this.getLogin= function(data){
-            
-           var dfd= $q.defer();
-            
-            
-            
+(function () {
+
+    function loginSvcFn($http, $q) {
+
+        this.getLogin = function (data) {
+
+            var dfd = $q.defer();
+
+
+
             $http({
-    method: 'POST',
-    url: "http://192.168.10.227:8080/test/test",
-    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-    transformRequest: function(obj) {
-        var str = [];
-        for(var p in obj)
-        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-        return str.join("&");
-    },
-    data: data
-}).then(function (response) {
-                
-               dfd.resolve(response);
-                
-                console.log(response); 
-                
+                method: 'POST',
+                url: "http://192.168.10.227:8080/test/test",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                transformRequest: function (obj) {
+                    var str = [];
+                    for (var p in obj)
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                    return str.join("&");
+                },
+                data: data
             })
-            
-            
-            .catch( function(){
-                
-                 dfd.reject(response);
+                .then(function (response) {
+
+                dfd.resolve(response);
+
                 console.log(response);
-                
+
             })
-            
-           
-            
+
+
+            .catch(function () {
+
+                dfd.reject(response);
+                console.log(response);
+
+            })
+
+
+
             return dfd.promise;
-            
+
         }
-        
-        
-        
-        
-        
-       
-        
+
+
+
+
+
+
+
     }
-    
-    
-    
-    
-    
+
+
+
+
+
     angular.module("menu")
-    .service("loginSvc", ["$http","$q", loginSvcFn]);
+        .service("loginSvc", ["$http", "$q", loginSvcFn]);
 })();
